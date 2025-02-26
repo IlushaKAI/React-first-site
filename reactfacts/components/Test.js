f40049bff2adcb4b1f3319e0fbab1feb3fe155c6
IlushaKAI <viktorovia@stud.kai.ru>
2025-02-26 08:39:25 +0300

    Добавил файлы проекта и дз

---
+++ reactfacts/components/Test.js
@@ -0,0 +1,8 @@
+
+    const h1 = document.createElement("h1")
+
+    h1.textContent = "This is imperative coding"
+
+    h1.className = "header"
+
+    document.getElementById("root").appendChild(h1)
